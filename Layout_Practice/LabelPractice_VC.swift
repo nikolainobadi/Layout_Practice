f9bc8cb837c4f438865b57c59f01b70196c1d6f0
Nikolai Rivas <nikolai.rivas@gmail.com>
2021-01-12 12:49:46 -0800

    added label practice comments

--- Layout_Practice/LabelPractice_VC.swift
+++ Layout_Practice/LabelPractice_VC.swift
@@ -15,25 +15,60 @@ class LabelPractice_VC: UIViewController {
         
         label.text = "This is your example"
         label.textAlignment = .center
-        label.backgroundColor = .lightGray
+        label.backgroundColor = .darkGray
+        label.textColor = .white
         
         return label
     }()
     
 
-    // MARK: - ADD YOUR VIEWS HERE
+    // MARK: EASY View
+    let easyLabel: BoringLabel = {
+        let label = BoringLabel(20)
+        
+        // add text to label
+        
+        // make textAlignment
+        
+        // make background color purple
+        
+        // make text color green
+        
+        
+        return label
+    }()
+    
+    
+    // MARK: - DIFFICULT View
+    
+    // create difficultLabel: BoringLabel
+    
     
     
     // MARK: - DISPLAY SETUP
     func addAllSubviews() {
         view.addSubview(exampleLabel)
+        
+        // add easyLabel to view
+        
+        // add difficultLabel to view
+        
     }
     
     func addContraints() {
         exampleLabel.anchor(view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, right: view.rightAnchor, topConstant: 10, leftConstant: 10, rightConstant: 10)
+        
+        // add space around easyLabel
+        // make top disance 50 (topConstant)
+        // leftConstant and rightConstant 20
+        // bottom and bottomConstant are not needed, delete
+        easyLabel.anchor(exampleLabel.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0)
+        
+        
+        // add constraints for difficultLabel
+        // use left, bottom, right, and bottomConstant
+        // hint: anchor(left: _, bottom: _, right: _, bottomConstant: _)
+        
     }
-    
-    
-    // MARK: - EXAMPLE ACTION
 }
 
